bc57492447b426c183c1873a80c066d481457340
borshchevsky <3410914@gmail.com>
2023-03-16 20:42:42 +0300

    some changes

--- src/services/genre.py
+++ src/services/genre.py
@@ -3,10 +3,10 @@ from functools import lru_cache
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 
+from src.core.config import GENRES_SEARCH_FIELD
 from src.db.elastic import get_elastic
 from src.models.genre import Genre
 from src.services.base_service import BaseService
-from src.core.config import GENRES_SEARCH_FIELD
 
 
 class GenreService(BaseService):
@@ -14,11 +14,10 @@ class GenreService(BaseService):
         super().__init__(*args, **kwargs)
         self.model = Genre
         self.index = 'genres'
-        self.search_field = GENRES_SEARCH_FIELD
 
 
 @lru_cache()
 def get_genre_service(
         elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> GenreService:
-    return GenreService(elastic)
+    return GenreService(elastic, GENRES_SEARCH_FIELD)
