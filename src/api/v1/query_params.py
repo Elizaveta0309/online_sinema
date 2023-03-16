a36fc0606ab2b50320f5ed9efb910a36de939442
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-03-16 15:11:40 +0400

    Create ListQueryParams and SearchQueryParams

--- src/api/v1/query_params.py
+++ src/api/v1/query_params.py
@@ -3,13 +3,12 @@ from fastapi import Query
 from src.core.config import PAGE_SIZE
 
 
-class QueryParams:
+class ListQueryParams:
     def __init__(
             self,
             page_number: int = Query(1, description='Page number'),
             page_size: int = Query(PAGE_SIZE, description='Page size'),
-            sort: str = Query('uuid', description='Sort field'),
-
+            sort: str = Query('uuid', description='Sort field')
     ):
         self.page_number = page_number
         self.page_size = page_size
@@ -20,3 +19,15 @@ class QueryParams:
         asc = 'desc' if sort[0] == '-' else 'asc'
         sorting_field = sort.replace('-', '')
         return sorting_field, asc
+
+
+class SearchQueryParams:
+    def __init__(
+            self,
+            page_number: int = Query(1, description='Page number'),
+            page_size: int = Query(PAGE_SIZE, description='Page size'),
+            query: str = Query('star', description='Search value')
+    ):
+        self.page_number = page_number
+        self.page_size = page_size
+        self.query = query
