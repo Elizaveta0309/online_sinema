e11ccb22039de5a8968377ba323aeabebefad6ae
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-03-16 15:10:40 +0400

    Add search and point to router

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -2,16 +2,17 @@ from http import HTTPStatus
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from src.api.v1.query_params import QueryParams
+from src.api.v1.query_params import ListQueryParams, SearchQueryParams
 from src.models.base_model import Model
 from src.models.person import Person
 from src.services.person import PersonService, get_person_service
+from src.core.config import PERSONS_SEARCH_FIELD
 
 router = APIRouter()
 
 
 @router.get('/')
-async def persons(params: QueryParams = Depends(), person_service: PersonService = Depends(get_person_service)):
+async def persons(params: ListQueryParams = Depends(), person_service: PersonService = Depends(get_person_service)):
     return await person_service.get_list(params)
 
 
@@ -22,3 +23,7 @@ async def person_details(person_id: str, person_service: PersonService = Depends
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
 
     return person
+
+@router.get("/search/")
+async def search_persons(params: SearchQueryParams = Depends(), person_service: PersonService = Depends(get_person_service)):
+    return await person_service.search(params, PERSONS_SEARCH_FIELD)
