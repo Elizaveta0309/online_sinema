695ef04e1edda6aedfb07c5305d1f662c6dabb4b
Pavel Borshchevsky <BorshchevskyPA@pba.su>
2023-03-20 15:12:09 +0300

    gunicorn

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ./wait-for-it.sh postgres:5432 -t 15
 ./wait-for-it.sh redis:6379 -t 15
 ./wait-for-it.sh elasticsearch:9200 -t 30
-uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+gunicorn src.main:app --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
