548ac53b1b104cc4ca4b0089b7c82e453db92706
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-03-24 14:34:57 +0400

    Add persons settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,9 @@
 from tests.testdata.movies_testdata.movies_mapping import MOVIES_MAPPING
-from tests.testdata.genres_testdata.genres_mapping import GENRES_MAPPING
 from tests.testdata.movies_testdata.movies_index_data import MOVIES_DATA
+from tests.testdata.genres_testdata.genres_mapping import GENRES_MAPPING
 from tests.testdata.genres_testdata.genres_index_data import GENRES_DATA
+from tests.testdata.persons_testdata.persons_mapping import PERSONS_MAPPING
+from tests.testdata.persons_testdata.persons_index_data import PERSONS_DATA
 from pydantic import BaseSettings, Field
 
 
@@ -17,6 +19,10 @@ class TestSettings(BaseSettings):
     genres_index: str = 'genres'
     genres_index_mapping: dict = GENRES_MAPPING
     genres_data: list = GENRES_DATA
+
+    persons_index: str = 'persons'
+    persons_index_mapping: dict = PERSONS_MAPPING
+    persons_data: list = PERSONS_DATA
 
     redis_host: str = 'redis-test:6379'
     service_url: str = 'http://localhost:8000'
