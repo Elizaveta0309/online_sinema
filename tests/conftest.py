fa150c49cbc02ad5ef5427cc29438591f57c6422
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-03-24 12:58:35 +0400

    Update commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,42 +2,47 @@ import json
 from typing import List
 
 import pytest
-from elasticsearch import AsyncElasticsearch, BadRequestError
+from aiohttp import ClientSession
+from elasticsearch import AsyncElasticsearch, RequestError
 
 from tests.settings import test_settings
-from tests.testdata.es_mapping import mapping
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 async def es_client():
     client = AsyncElasticsearch(hosts=test_settings.es_host)
     yield client
     await client.close()
 
 
+@pytest.fixture()
+async def client_session():
+    session = ClientSession()
+    yield session
+    await session.close()
+
+
+
+
 @pytest.fixture
 def es_write_data(es_client):
-    async def inner(data: List[dict]):
+    async def inner(data: List[dict], index: str, mapping: dict):
         bulk_query = []
         for row in data:
             bulk_query.extend([
-                json.dumps({'index': {'_index': test_settings.es_index, '_id': row[test_settings.es_id_field]}}),
+                json.dumps({'index': {'_index': index, '_id': row[test_settings.es_id_field]}}),
                 json.dumps(row)
             ])
 
         str_query = '\n'.join(bulk_query) + '\n'
-
-        es_client = AsyncElasticsearch(hosts=test_settings.es_host)
-
         try:
-            await es_client.indices.create(index='movies', body=mapping)
-        except BadRequestError as e:
+            await es_client.indices.create(index=index, body=mapping)
+        except RequestError as e:
             if e.error != 'resource_already_exists_exception':
                 raise e
 
-        response = await es_client.bulk(operations=str_query, refresh=True)
+        response = await es_client.bulk(str_query, refresh=True)
 
-        await es_client.close()
         if response['errors']:
             raise Exception('Ошибка записи данных в Elasticsearch')
 
@@ -48,6 +53,13 @@ def es_write_data(es_client):
 def es_delete_index(es_client):
     async def inner(index):
         await es_client.indices.delete(index=index, ignore=[400, 404])
-        await es_client.close()
+    return inner
 
+
+@pytest.fixture()
+def make_get_request(client_session):
+    async def inner(endpoint: str, query_data: dict = None):
+        url = test_settings.service_url + endpoint
+        response = await client_session.get(url, params=query_data)
+        return response
     return inner
