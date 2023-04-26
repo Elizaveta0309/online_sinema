30bd43dbb11bb1b3e45aeff44e9c022fa3d16a37
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-04-26 18:42:32 +0400

    Add flasgger support

--- auth/app.py
+++ auth/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 # noinspection PyUnresolvedReferences
 from api.v1.views import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
