929fcf93eb71a7619d0d0f584980a5ea21db4d03
Adedal513 <adedalmoroz@gmail.com>
2023-04-29 13:19:34 +0300

    Добавлен троттлинг

--- auth/app.py
+++ auth/app.py
@@ -1,9 +1,22 @@
 from flasgger import Swagger
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from config import settings
 
 from middleware import ExceptionHandlerMiddleware
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri=f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}',
+    strategy='fixed-window',
+    default_limits=[
+        f'{settings.REQUEST_NUM_LIMIT} per {settings.REQUEST_TIME_LIMIT}'
+    ]
+)
+
 swagger = Swagger(app)
 app.wsgi_app = ExceptionHandlerMiddleware(app.wsgi_app)
 
