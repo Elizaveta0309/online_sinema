914281f181c286d050ad4607a7f4446acb2a7bc8
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-04-30 15:07:00 +0400

    Add jaeger configuration

--- auth/app.py
+++ auth/app.py
@@ -1,10 +1,44 @@
 from flasgger import Swagger
 from flask import Flask
+from flask_request_id_header.middleware import RequestID
+
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 from middleware import ExceptionHandlerMiddleware
 
+resource = Resource(attributes={
+    SERVICE_NAME: "Auth"
+})
+
+
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name='jaeger',
+                agent_port=6831
+            )
+        )
+    )
+
+
+configure_tracer()
+
 app = Flask(__name__)
+
+FlaskInstrumentor().instrument_app(app)
+
 swagger = Swagger(app)
+
+app.config['REQUEST_ID_UNIQUE_VALUE_PREFIX'] = 'AUTH-'
+RequestID(app)
+
 app.wsgi_app = ExceptionHandlerMiddleware(app.wsgi_app)
 
 # noinspection PyUnresolvedReferences
--- auth/permissions.py
+++ auth/permissions.py
@@ -6,45 +6,53 @@ from utils.utils import is_token_expired, jwt_decode
 
 from db.models import Role, User
 
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
+
 
 def jwt_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        blacklist = kwargs.get('blacklist')
-        access_token = request.cookies.get('token')
-        refresh_token = request.cookies.get('refresh')
+        with tracer.start_as_current_span('check_jwt'):
+            blacklist = kwargs.get('blacklist')
+            access_token = request.cookies.get('token')
+            refresh_token = request.cookies.get('refresh')
 
-        if not (access_token and refresh_token):
-            return (
-                jsonify({'error': 'token is not provided'}),
-                HTTPStatus.FORBIDDEN
-            )
+            if not (access_token and refresh_token):
+                return (
+                    jsonify({'error': 'token is not provided'}),
+                    HTTPStatus.FORBIDDEN
+                )
 
-        if blacklist.is_expired(access_token) or is_token_expired(access_token):
-            return (
-                jsonify({'error': 'token is already blacklisted or expired'}),
-                HTTPStatus.BAD_REQUEST
-            )
+            if blacklist.is_expired(access_token) or is_token_expired(access_token):
+                return (
+                    jsonify({'error': 'token is already blacklisted or expired'}),
+                    HTTPStatus.BAD_REQUEST
+                )
 
-        return f(*args, **kwargs)
+            return f(*args, **kwargs)
+
     return decorator
 
 
 def admin_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        token = request.cookies.get('token')
-        user_id = jwt_decode(token).get('user_id')
-        user: User = User.query.filter_by(id=user_id).first()
-        role = Role.query.filter_by(id=user.role).first()
+        with tracer.start_as_current_span('check_role'):
+            token = request.cookies.get('token')
+            user_id = jwt_decode(token).get('user_id')
+            user: User = User.query.filter_by(id=user_id).first()
+            role = Role.query.filter_by(id=user.role).first()
 
-        if not user:
-            return jsonify({'error': 'not found'}), HTTPStatus.NOT_FOUND
-        if role.title not in ['admin', 'superadmin']:
-            return (
-                jsonify({'error': 'admin rights required'}),
-                HTTPStatus.BAD_REQUEST
-            )
+            if not user:
+                return jsonify({'error': 'not found'}), HTTPStatus.NOT_FOUND
+            if role.title not in ['admin', 'superadmin']:
+                return (
+                    jsonify({'error': 'admin rights required'}),
+                    HTTPStatus.BAD_REQUEST
+                )
 
-        return f(*args, **kwargs)
+            return f(*args, **kwargs)
+
     return decorator
