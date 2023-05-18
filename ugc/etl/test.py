50dc1ea4807eb0ca2ae8ae1e6f8dd81086d712b6
Николай Мороз <adedalmoroz@gmail.com>
2023-05-18 16:11:21 +0300

    Removed a tmp file

--- ugc/etl/test.py
+++
@@ -1,8 +0,0 @@
-from clickhouse_driver import Client
-from pre_start import check_clickhouse_inited
-
-c = Client(host='localhost')
-
-check_clickhouse_inited(c)
-print(c.execute("SELECT * FROM viewed_progress"))
-c.disconnect()
