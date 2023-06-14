773e887531472dc2bac68c53f462d033c5afe95f
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-06-15 00:26:16 +0400

    Add service

--- notifications/api/src/api/v1/notifications.py
+++ notifications/api/src/api/v1/notifications.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
-#from src.services.time_code import TimeCodeService, get_time_code_service
-
+from src.services.notifications import NotificationsService, get_notifications_service
 from .params import LikeParams, NewFilmsParams, SomeEventParams
 
 router = APIRouter()
@@ -12,24 +11,24 @@ router = APIRouter()
              response_description='Like notification')
 async def send_like_notification(
     params: LikeParams = Depends(),
-    #service: TimeCodeService = Depends(get_time_code_service)
+    service: NotificationsService = Depends(get_notifications_service)
 ):
-    return await service.set_time_code(params)
+    return await service.send_notification(params)
 
 @router.post('/',
              description='Метод позволяет позволяет положить в очередь событие о еженедельной рассылке о новых фильмах на платформе',
              response_description='Monthly notifications')
 async def send_monthly_notifications(
     params: NewFilmsParams= Depends(),
-    #service: TimeCodeService = Depends(get_time_code_service)
+    service: NotificationsService = Depends(get_notifications_service)
 ):
-    return await service.set_time_code(params)
+    return await service.send_notification(params)
 
 @router.post('/',
              description='Метод позволяет отправить положить в очередь событие о ручном уведомлении от менеджера',
              response_description='Notification by manager')
 async def send_personalized_notification(
     params: SomeEventParams = Depends(),
-    #service: TimeCodeService = Depends(get_time_code_service)
+    service: NotificationsService = Depends(get_notifications_service)
 ):
-    return await service.set_time_code(params)
+    return await service.send_notification(params)
