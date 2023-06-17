7d6325101db9ef02b1a539fb1e739d0639019f42
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-06-17 23:15:04 +0400

    Remove something

--- notifications/api/src/api/v1/params.py
+++ notifications/api/src/api/v1/params.py
@@ -1,81 +1,28 @@
-from typing import List
-
 from fastapi import Query
 
-
-class LikeParams:
+class NewFilmParams:
     def __init__(
             self,
             transport: str = Query(
                 default='email'
             ),
             event_type: str = Query(
-                default='notification_about_like',
+                default='notification_about_new_film',
                 description='Describes type of event',
             ),
-            user_id: str = Query(
-                default='user_id_for_example',
-                description='User ID',
-            ),
-            review_id: str = Query(
-                default='review_id_for_example',
-                description='Review ID',
-            ),
-            liker_id: str = Query(
-                default='liker_id_for_example',
-                description='Liker ID',
-            )
-    ):
-        self.transport = transport
-        self.event_type = event_type
-        self.user_id = user_id
-        self.review_id = review_id
-        self.liker_id = liker_id
-
-class NewFilmsParams:
-    def __init__(
-            self,
-            transport: str = Query(
-                default='email'
-            ),
-            event_type: str = Query(
-                default='notification_about_new_films',
-                description='Describes type of event',
-            ),
-            users_id: List[str] = Query(
-                description='Users IDs',
-            ),
-            films_id: List[str] = Query(
-                description='Films IDs',
-            ),
-    ):
-        self.transport = transport
-        self.event_type = event_type
-        self.users_id = users_id
-        self.films_id = films_id
-
-
-class SomeEventParams:
-    def __init__(
-            self,
-            transport: str = Query(
-                default='email'
-            ),
-            event_type: str = Query(
-              default='notification_from_admin_panel',
-              description='Describes type of event',
+            template_id: str = Query(
+                description='Template ID'
             ),
             user_id: str = Query(
-                default='user_id_for_example',
-                description='User ID',
+                description='User ID'
             ),
-            text: str = Query(
-                default='some personalized subscription offer',
-                decsription='Text of personalized letter for user'
+            film_id: str = Query(
+                description='Films ID',
             ),
-
     ):
         self.transport = transport
         self.event_type = event_type
-        self.user_id = user_id
-        self.text = text
+        self.template_id = template_id
+        self.users_id = user_id
+        self.films_id = film_id
+
